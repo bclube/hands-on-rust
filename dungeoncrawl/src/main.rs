b9f7c7b4ebb8740025dbc4be069a53c8ef096c90
Brian Lubeski <bclube@gmail.com>
2021-12-12 19:19:47 -0500

    Chapter 8 - Give entities names; implement tool tips

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -40,7 +40,7 @@ impl State {
         let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
-        spawn_player(&mut ecs, map_builder.player_start);
+        spawn_player(&mut ecs, map_builder.player_start, "Adventurer".to_string());
         map_builder
             .rooms
             .iter()
@@ -74,6 +74,8 @@ impl GameState for State {
         ctx.set_active_console(2);
         ctx.cls();
         self.resources.insert(ctx.key);
+        ctx.set_active_console(0);
+        self.resources.insert(Point::from_tuple(ctx.mouse_pos()));
         let current_state = *self.resources.get::<TurnState>().unwrap();
         let scheduler = match current_state {
             TurnState::AwaitingInput => &mut self.input_systems,
@@ -96,7 +98,7 @@ fn main() -> BError {
         .with_font("terminal8x8.png", 8, 8)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
-        .with_simple_console_no_bg(DISPLAY_WIDTH * 2, DISPLAY_HEIGHT * 2, "terminal8x8.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH * 4, DISPLAY_HEIGHT * 4, "terminal8x8.png")
         .build()?;
 
     main_loop(context, State::new())
--- dungeoncrawl/src/spawner.rs
+++ dungeoncrawl/src/spawner.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
 
-pub fn spawn_player(ecs: &mut World, pos: Point) {
+pub fn spawn_player(ecs: &mut World, pos: Point, name: String) {
     ecs.push((
         Player,
         pos,
@@ -12,23 +12,35 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             current: 20,
             max: 20,
         },
+        Name(name),
     ));
 }
 
 pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
-    let char = match rng.range(0, 4) {
-        0 => 'E',
-        1 => 'O',
-        2 => 'o',
-        _ => 'g',
+    let (hp, name, glyph) = match rng.roll_dice(1, 10) {
+        1..=8 => goblin(),
+        _ => orc(),
     };
     ecs.push((
         Enemy,
         pos,
         Render {
             color: ColorPair::new(WHITE, BLACK),
-            glyph: to_cp437(char),
+            glyph,
         },
         MovingRandomly,
+        Health {
+            current: hp,
+            max: hp,
+        },
+        Name(name),
     ));
+}
+
+fn goblin() -> (i32, String, FontCharType) {
+    (1, "Goblin".to_string(), to_cp437('g'))
+}
+
+fn orc() -> (i32, String, FontCharType) {
+    (2, "Orc".to_string(), to_cp437('o'))
 }
