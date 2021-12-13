52d11ddf5b1378e4dd5bc30cac305c6c84844a23
Brian Lubeski <bclube@gmail.com>
2021-12-13 00:46:48 -0500

    Chapter 8 - Enemies can attack

--- dungeoncrawl/src/systems/random_move.rs
+++ dungeoncrawl/src/systems/random_move.rs
@@ -3,12 +3,19 @@ use crate::prelude::*;
 #[system]
 #[write_component(Point)]
 #[read_component(MovingRandomly)]
+#[read_component(Health)]
+#[read_component(Player)]
 pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer, #[resource] map: &Map) {
+    let others = <(Entity, &Point)>::query()
+        .filter(component::<Health>())
+        .iter(ecs)
+        .map(|(entity, pos)| (*entity, pos))
+        .collect::<Vec<(Entity, &Point)>>();
     <(Entity, &Point, &MovingRandomly)>::query()
-        .iter_mut(ecs)
+        .iter(ecs)
         .for_each(|(entity, pos, _)| {
             let mut rng = RandomNumberGenerator::new();
-            loop {
+            let destination = loop {
                 let destination = match rng.range(0, 4) {
                     0 => Point::new(-1, 0),
                     1 => Point::new(1, 0),
@@ -16,15 +23,31 @@ pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer, #[resource]
                     _ => Point::new(0, 1),
                 } + *pos;
                 if map.can_enter_tile(destination) {
-                    commands.push((
-                        (),
-                        WantsToMove {
-                            entity: *entity,
-                            destination,
-                        },
-                    ));
-                    break;
+                    break destination;
                 }
+            };
+            if let Some(victim) = others.iter().find_map(|(entity, pos)| {
+                if **pos == destination {
+                    Some(entity)
+                } else {
+                    None
+                }
+            }) {
+                commands.push((
+                    (),
+                    WantsToAttack {
+                        attacker: *entity,
+                        victim: *victim,
+                    },
+                ));
+            } else {
+                commands.push((
+                    (),
+                    WantsToMove {
+                        entity: *entity,
+                        destination,
+                    },
+                ));
             }
         });
 }
