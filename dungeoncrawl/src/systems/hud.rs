8cea6868c01306dbb835e2596c1bca1d26ea285d
Brian Lubeski <bclube@gmail.com>
2021-12-15 23:52:05 -0500

    Chapter 8 - Add ability to wait to regain health

--- dungeoncrawl/src/systems/hud.rs
+++ dungeoncrawl/src/systems/hud.rs
@@ -4,24 +4,24 @@ use crate::prelude::*;
 #[read_component(Health)]
 #[read_component(Player)]
 pub fn hud(ecs: &SubWorld) {
-    let mut health_query = <&Health>::query().filter(component::<Player>());
-    let player_health = health_query.iter(ecs).next().unwrap();
+    let (current_health, max_health) = <&Health>::query()
+        .filter(component::<Player>())
+        .iter(ecs)
+        .next()
+        .map_or((0, 1), |h| (h.current, h.max));
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(2);
     draw_batch.print_centered(1, "Explore the Dungeon. Cursor keys to move.");
     draw_batch.bar_horizontal(
         Point::zero(),
         SCREEN_WIDTH * 2,
-        player_health.current,
-        player_health.max,
+        current_health,
+        max_health,
         ColorPair::new(RED, BLACK),
     );
     draw_batch.print_color_centered(
         0,
-        format!(
-            " Health: {} / {} ",
-            player_health.current, player_health.max
-        ),
+        format!(" Health: {} / {} ", current_health, max_health),
         ColorPair::new(WHITE, RED),
     );
     draw_batch.submit(10000).expect("Batch error");
--- dungeoncrawl/src/systems/player_input.rs
+++ dungeoncrawl/src/systems/player_input.rs
@@ -1,8 +1,10 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -16,46 +18,51 @@ pub fn player_input(
         Some(VirtualKeyCode::Left) => Point::new(-1, 0),
         Some(VirtualKeyCode::Right) => Point::new(1, 0),
         Some(VirtualKeyCode::Space) => {
+            <&mut Health>::query()
+                .filter(component::<Player>())
+                .iter_mut(ecs)
+                .for_each(|health| health.current = health.max.min(health.current + 1));
             *turn_state = TurnState::PlayerTurn;
             return;
         }
         _ => return,
     };
-    let (player_entity, destination) = <(Entity, &Point)>::query()
+    if let Some((player_entity, destination)) = <(Entity, &Point)>::query()
         .filter(component::<Player>())
         .iter(ecs)
         .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
-        .unwrap();
-    if !map.can_enter_tile(destination) {
-        return;
-    }
-    *turn_state = TurnState::PlayerTurn;
-    if let Some(enemy) = <(Entity, &Point)>::query()
-        .filter(component::<Enemy>())
-        .iter(ecs)
-        .filter(|(_, pos)| **pos == destination)
-        .find_map(|(entity, pos)| {
-            if *pos == destination {
-                Some(entity)
-            } else {
-                None
-            }
-        })
     {
-        commands.push((
-            (),
-            WantsToAttack {
-                attacker: player_entity,
-                victim: *enemy,
-            },
-        ));
-    } else {
-        commands.push((
-            (),
-            WantsToMove {
-                entity: player_entity,
-                destination,
-            },
-        ));
+        if !map.can_enter_tile(destination) {
+            return;
+        }
+        *turn_state = TurnState::PlayerTurn;
+        if let Some(enemy) = <(Entity, &Point)>::query()
+            .filter(component::<Enemy>())
+            .iter(ecs)
+            .filter(|(_, pos)| **pos == destination)
+            .find_map(|(entity, pos)| {
+                if *pos == destination {
+                    Some(entity)
+                } else {
+                    None
+                }
+            })
+        {
+            commands.push((
+                (),
+                WantsToAttack {
+                    attacker: player_entity,
+                    victim: *enemy,
+                },
+            ));
+        } else {
+            commands.push((
+                (),
+                WantsToMove {
+                    entity: player_entity,
+                    destination,
+                },
+            ));
+        }
     }
 }
